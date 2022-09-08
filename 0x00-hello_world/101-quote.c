0a479e9792f0094dd381749492e34438c6f1d25b
Great Ononiwu <ononiwugreat@yahoo.com>
2022-09-08 16:53:35 -0700

    iop

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - prints the size of various types
- * Return: 0 if exited properly, non-zero otherwise
+ * main - Entry point
+ * Return:1
  */
 int main(void)
 {
-		printf("Size of a char: %d byte(s)\n", sizeof(char));
-		printf("Size of an int: %d byte(s)\n", sizeof(int));
-		printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-		printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-		printf("Size of a float: %d byte(s)\n", sizeof(float));
-		return (0);
+	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, ch, sizeof(ch));
+	return (1);
 }
