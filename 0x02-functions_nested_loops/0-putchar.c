42919e7ec09f339f83b3d7e6b5a298abeeb6fb8a
Great Ononiwu <ononiwugreat@yahoo.com>
2022-09-14 00:24:07 -0700

    po

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,5 @@
 #include "main.h"
 
-
-
-
-
 /**
  *
  * main - Entry point
@@ -14,32 +10,17 @@
  *
  */
 
-
-
 int main(void)
-
 {
-
 	_putchar('_');
-
 	_putchar('p');
-
 	_putchar('u');
-
 	_putchar('t');
-
 	_putchar('c');
-
 	_putchar('h');
-
 	_putchar('a');
-
 	_putchar('r');
-
 	_putchar('\n');
 
-
-
 	return (0);
-
 }
