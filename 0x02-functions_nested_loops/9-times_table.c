3486a0c9375ba387ec72d60c56de8912565489f8
Great Ononiwu <ononiwugreat@yahoo.com>
2022-09-14 02:54:34 -0700

    rq

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,28 +16,18 @@ void times_table(void)
 
 	int r, c, d;
 
-
-
 	for (r = 0; r <= 9; r++)
-
 	{
-
 		_putchar('0');
-
 		_putchar(',');
-
 		_putchar(' ');
 
 	for (c = 1; c <= 9; c++)
-
 	{
-
 		d = (r * c);
 
 		if ((d / 10) > 0)
-
 		{
-
 			_putchar((d / 10) + '0');
 
 		}
@@ -45,7 +35,6 @@ void times_table(void)
 		else
 
 		{
-
 			_putchar(' ');
 
 		}
@@ -53,11 +42,8 @@ void times_table(void)
 			_putchar((d % 10) + '0');
 
 		if (c < 9)
-
 		{
-
 			_putchar(',');
-
 			_putchar(' ');
 		}
 
