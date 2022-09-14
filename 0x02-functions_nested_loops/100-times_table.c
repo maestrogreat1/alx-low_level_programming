0bda199916fd4c536dc6bac36b9526e303dcaaae
Great Ononiwu <ononiwugreat@yahoo.com>
2022-09-14 04:12:35 -0700

    joy

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * Description: print times table - prints the n times table starting with 0 
- *
+ * print_times_table - prints the n times table starting with 0 
  * @n: number of the times table
- *
  */
 
 void print_times_table(int n)
