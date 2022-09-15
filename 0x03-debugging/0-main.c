e7d800de1aa698ab96b3907b601c52b1cfdab5e1
Great Ononiwu <ononiwugreat@yahoo.com>
2022-09-15 13:37:35 -0700

    gol

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
-
 /**
  * main - Debugging.
- *
  * Return: 0
  */
 
@@ -11,11 +9,13 @@
 int main(void)
 
 {
+
 	int i;
 
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 
 }
