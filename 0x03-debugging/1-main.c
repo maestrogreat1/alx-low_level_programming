31a42e5e4fabbbc8f52a232c57cc0e3dc26b196a
Great Ononiwu <ononiwugreat@yahoo.com>
2022-09-15 14:33:07 -0700

    gol

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,6 +2,7 @@
 
 /**
  * main - causes an infinite loop
+ *
  * Return: 0
  */
 
@@ -14,10 +15,10 @@ int main(void)
 
 	i = 0;
 
-       /**
+       /*
 	* while (i < 10)
 	* {
-	* 	putchar(i);
+	* putchar(i);
 	* }
 	*/
 	printf("Infinite loop avoided! \\o/\n");
