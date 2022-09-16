4cb57f4d5655425f83c804c9253012157b160666
Great Ononiwu <ononiwugreat@yahoo.com>
2022-09-15 21:11:38 -0700

    you

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,6 @@
 /**
  * print_numbers - prints the numbers, from 0 to 9
  * followed by a new line
- * Return: 0
 */
 
 void print_numbers(void)
@@ -16,6 +15,4 @@ void print_numbers(void)
 	}
 
 	_putchar('\n');
-
-	return (0);
 }
