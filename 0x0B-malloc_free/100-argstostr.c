da2f798875b422d5551c57820abb483e2cc1e477
Great Ononiwu <ononiwugreat@yahoo.com>
2022-10-05 09:52:02 -0700

    poll

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -22,7 +22,7 @@ char *argstostr(int ac, char **av)
 			return (NULL);
 		for (j = 0; av[i][j] != '\0'; j++)
 			c++;
-			c++;
+		c++;
 	}
 	aout = malloc((c + 1) * sizeof(char));
 	if (aout == NULL)
